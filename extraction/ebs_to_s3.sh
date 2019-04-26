c78de78238735c08231f562d260bf5d065be210f
mudabirkabir <mudabirkabir@gmail.com>
2019-04-26 00:30:25 -0700

    adding shell script to mv files to S3

---
+++ extraction/ebs_to_s3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+EBS_FOLDER=$1
+S3_PATH=$2
+
+
+if [ -d !$EBS_FOLDER]; then
+	echo "EBS mounted folder not found"
+	exit -1
+fi
+
+
+for FILE in `ls $EBS_FOLDER/airline_ontime/*/*.zip`; do
+	for CSV_NAME in `unzip -l $FILE  | grep csv | tr -s ' ' | cut -d ' ' -f4`; do
+		unzip -p $FILE $CSV_NAME | aws s3 mv - $S3_PATH
+		echo "Moving $CSV_NAME to $S3_PATH"
+	done 
+done
