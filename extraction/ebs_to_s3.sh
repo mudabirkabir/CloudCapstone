d00fe280fde211ea56ec05d0a26f82fdf45ae06d
root <root@ip-172-31-33-50.ec2.internal>
2019-04-26 08:37:16 +0000

    changed mv to cp and fixed an extra space

--- extraction/ebs_to_s3.sh
+++ extraction/ebs_to_s3.sh
@@ -3,7 +3,7 @@ EBS_FOLDER=$1
 S3_PATH=$2
 
 
-if [ -d !$EBS_FOLDER]; then
+if [ -d !$EBS_FOLDER ]; then
 	echo "EBS mounted folder not found"
 	exit -1
 fi
@@ -11,7 +11,7 @@ fi
 
 for FILE in `ls $EBS_FOLDER/airline_ontime/*/*.zip`; do
 	for CSV_NAME in `unzip -l $FILE  | grep csv | tr -s ' ' | cut -d ' ' -f4`; do
-		unzip -p $FILE $CSV_NAME | aws s3 mv - $S3_PATH/$CSV_NAME
+		unzip -p $FILE $CSV_NAME | aws s3 cp - $S3_PATH/$CSV_NAME
 		echo "Moving $CSV_NAME to $S3_PATH"
 	done 
 done
