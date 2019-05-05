3faaf3d1088589ee498205756a0e482f8e153bc7
mudabirkabir <mudabirkabir@gmail.com>
2019-05-05 15:03:26 -0700

    First logic for top 10 airports

--- spark/g1_1.py
+++ spark/g1_1.py
@@ -2,12 +2,12 @@ import os
 from pyspark import SparkConf, SparkContext
 import boto3
 
-
+s3Bucket = 'mudabircapstone'
 def getFileNames():
 	
 	s3 = boto3.client('s3')
 	keys = []
-	resp = s3.list_objects_v2(Bucket='mudabircapstone')
+	resp = s3.list_objects_v2(Bucket=s3Bucket)
 	for obj in resp['Contents']:
 		keys.append(obj['Key'])
 	
@@ -26,6 +26,27 @@ allFiles = getFileNames()
 for inputFile in allFiles:
 	print(inputFile)
 
-#rdd = sc.textFile(inputFile)
+#rdd = sc.textFile('s3://%s' % s3Bucket)
+rdd = sc.textFile('s3://%s/Sample.csv' % s3Bucket)
+
+airports = rdd.map(lambda line: line.split(',')).flatmap(lambda row: [row[11],row[17]])
+
+counts = airports.map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y)
+
+#result = counts.map(lambda x: (x[1],x[0])).sortByKey(ascending=False).map(lambda y: (y[1],y[0]))
+
+#result = counts.sortBy(lambda x: x[1], ascending=False).takeOrdered(10,key=lambda x:-x[1])
+result = counts.takeOrdered(10,key=lambda x:-x[1])
+
+output = result.collect()
+
+for pair in output:
+	print('%1' % pair)
+
+#Check what is parallelize
+# >>> sc.parallelize(tmp).sortBy(lambda x: x[0]).collect()
+
+
 
 sc.stop()
+                    
