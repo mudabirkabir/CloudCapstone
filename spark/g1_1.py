d907caee55eab7ede1fc314c62f511610ece03da
root <root@ip-172-31-32-61.ec2.internal>
2019-05-06 06:07:41 +0000

    tested on sample files

--- spark/g1_1.py
+++ spark/g1_1.py
@@ -2,14 +2,14 @@ import os
 from pyspark import SparkConf, SparkContext
 import boto3
 
-s3Bucket = 'mudabircapstone'
+s3Bucket = 'mudabircapstonesample'
 def getFileNames():
 	
 	s3 = boto3.client('s3')
 	keys = []
 	resp = s3.list_objects_v2(Bucket=s3Bucket)
 	for obj in resp['Contents']:
-		keys.append(obj['Key'])
+		keys.append('s3://%s/%s' %(s3Bucket,obj['Key']))
 	
 	return keys
 
@@ -30,7 +30,7 @@ allFiles = getFileNames()
 #rdd = sc.textFile('s3://%s/Sample.csv' % s3Bucket)
 rdd = sc.textFile(','.join(allFiles))
 
-airports = rdd.map(lambda line: line.split(',')).flatMap(lambda row: [row[11],row[17]])
+airports = rdd.map(lambda line: line.split(',')).flatMap(lambda row: [row[11],row[18]])
 
 counts = airports.map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y)
 
