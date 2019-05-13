7ea912f1d3a35cf9010bf9f54d0150f566f7af93
mudabirkabir <mudabirkabir@gmail.com>
2019-05-12 22:11:08 -0700

    changed to double quotes

--- spark/g2_1_query.py
+++ spark/g2_1_query.py
@@ -7,7 +7,7 @@ from boto3.dynamodb.conditions import Key
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamodb.Table('Top10Carriers')
 
-task1_queries = ['CMI', 'BWI', 'MIA', 'LAX', 'IAH', 'SFO']
+task1_queries = ["CMI", "BWI", "MIA", "LAX", "IAH", "SFO"]
 
 for origin in task1_queries:
     resp = table.query(
