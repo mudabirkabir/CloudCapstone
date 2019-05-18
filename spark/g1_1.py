0aab47ce1df2ba5c576a40ad196515a5675f307b
mudabirkabir <mudabirkabir@gmail.com>
2019-05-17 19:36:54 -0700

    code cleanup

--- spark/g1_1.py
+++ spark/g1_1.py
@@ -14,37 +14,23 @@ def getFileNames():
 	return keys
 
 
-
-
 conf = SparkConf()
 sc = SparkContext(conf = conf)
 
 allFiles = []
 allFiles = getFileNames()
 
-# #debugprint
-# for inputFile in allFiles:
-# 	print(inputFile)
-
-#rdd = sc.textFile('s3://%s//*' % s3Bucket)
-#rdd = sc.textFile('s3://%s/Sample.csv' % s3Bucket)
 rdd = sc.textFile(','.join(allFiles))
 
 airports = rdd.map(lambda line: line.split(',')).flatMap(lambda row: [row[11],row[18]])
 
 counts = airports.map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y)
 
-#result = counts.map(lambda x: (x[1],x[0])).sortByKey(ascending=False).map(lambda y: (y[1],y[0]))
 
-#result = counts.sortBy(lambda x: x[1], ascending=False).takeOrdered(10,key=lambda x:-x[1])
 result = counts.takeOrdered(10,key=lambda x:-x[1])
 
 for pair in result:
 	print(pair)
-
-#Check what is parallelize
-# >>> sc.parallelize(tmp).sortBy(lambda x: x[0]).collect()
-
 
 
 sc.stop()
