4e8b9c767a8cf5c779fea5f399b8aded18a425ad
mudabirkabir <mudabirkabir@gmail.com>
2019-05-26 22:51:33 -0700

    removing unused packages

--- sparkStreaming/s3_to_kafka_streamer.py
+++ sparkStreaming/s3_to_kafka_streamer.py
@@ -1,11 +1,7 @@
-import re
-
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
 from kafka import SimpleProducer, KafkaClient
 
-s3Bucket = 'mudabircapstonesample3'
 
 def notCancelled(row):
     try:
@@ -16,14 +12,14 @@ def notCancelled(row):
     except:
         return False
 
-def printResults(rdd):
+def printBatch(rdd):
     print "-----------------*******----------------------"
     for line in rdd.take(10):
         print line
 
 def streamOut(items):
     kafka = KafkaClient('172.31.40.107:9092,172.31.44.173:9092,172.31.34.192:9092')
-    producer = SimpleProducer(kafka, async=False)
+    producer = SimpleProducer(kafka)
     for item in items:
         producer.send_messages('test', item.encode())
 
@@ -37,7 +33,7 @@ rows = lines.map(lambda line: line.split(',')).filter(notCancelled)
 
 reqInfo = rows.map(lambda row: "|".join((row[4],row[6],row[10],row[11], row[18], row[25], row[27], row[38])))
 
-reqInfo.foreachRDD(lambda rdd: printResults(rdd))
+reqInfo.foreachRDD(lambda rdd: printBatch(rdd))
 
 reqInfo.foreachRDD(lambda rdd: rdd.foreachPartition(streamOut))
 
