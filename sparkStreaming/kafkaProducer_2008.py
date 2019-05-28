a8a686f9e280fe374161a1d4203407fd1492776b
mudabirkabir <mudabirkabir@gmail.com>
2019-05-27 20:25:03 -0700

    kafka producers

---
+++ sparkStreaming/kafkaProducer_2008.py
@@ -0,0 +1,14 @@
+import os
+from kafka import SimpleProducer, KafkaClient
+
+# To send messages synchronously
+kafka = KafkaClient('172.31.40.107:9092,172.31.44.173:9092,172.31.34.192:9092')
+producer = SimpleProducer(kafka)
+
+
+file = open('/home/hadoop/output/filteredData_2008', 'r')
+
+for line in file:
+    producer.send_messages('airports_2008', line)
+
+file.close()
