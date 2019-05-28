dc15728d2540428a6739e361dc1e9dc59f3dd4b5
root <root@ip-172-31-35-36.ec2.internal>
2019-05-28 02:06:22 +0000

    sample filtered data

--- sparkStreaming/s3_to_kafka_streamer_kafkaProducer.py
+++ sparkStreaming/s3_to_kafka_streamer_kafkaProducer.py
@@ -1,7 +1,7 @@
 import os
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-from kafka import KafkaProducer
+#from kafka import KafkaProducer
 
 
 def notCancelled(row):
@@ -33,7 +33,7 @@ rows = lines.map(lambda line: line.replace('"', '')).map(lambda line: line.split
 reqInfo = rows.map(lambda row: "|".join((row[0],row[2],row[3],row[4],row[6],row[10],row[11], row[18], row[25], row[27], row[38])))
 
 f = open("filteredData","w+")
-reqInfo.foreachRDD(lambda rdd: writeToFile(rdd))
+reqInfo.foreachRDD(lambda rdd: writeToFile(rdd,f))
 
 #reqInfo.foreachRDD(lambda rdd: rdd.foreachPartition(streamOut))
 
