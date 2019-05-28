adba4a7d2a468b9bdb13c9ac66f8bbb36193b47e
mudabirkabir <mudabirkabir@gmail.com>
2019-05-27 18:43:04 -0700

    decoupled kafka producer

--- sparkStreaming/s3_to_kafka_streamer_kafkaProducer.py
+++ sparkStreaming/s3_to_kafka_streamer_kafkaProducer.py
@@ -1,3 +1,4 @@
+import os
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from kafka import KafkaProducer
@@ -12,15 +13,14 @@ def notCancelled(row):
     except:
         return False
 
-def printBatch(rdd):
-    print "-----------------*******----------------------"
-    for line in rdd.take(10):
-        print line
+def writeToFile(rdd,f):
+    for line in rdd.collect():
+        f.write(line+"\n")
 
-def streamOut(items):
-    producer = KafkaProducer(bootstrap_servers=['172.31.40.107:9092','172.31.44.173:9092','172.31.34.192:9092'])
-    for item in items:
-        producer.send('test', item)
+# def streamOut(items):
+#     producer = KafkaProducer(bootstrap_servers=['172.31.40.107:9092','172.31.44.173:9092','172.31.34.192:9092'])
+#     for item in items:
+#         producer.send('test', item)
 
 sc = SparkContext(appName="streamer")
 sc.setLogLevel("ERROR")
@@ -28,13 +28,14 @@ ssc = StreamingContext(sc, 3)
 
 lines = ssc.textFileStream("hdfs:///user/root/input")
 
-rows = lines.map(lambda line: line.split(',')).filter(notCancelled)
+rows = lines..map(lambda line: line.replace('"', '')).map(lambda line: line.split(',')).filter(notCancelled)
 
 reqInfo = rows.map(lambda row: "|".join((row[0],row[2],row[3],row[4],row[6],row[10],row[11], row[18], row[25], row[27], row[38])))
 
-reqInfo.foreachRDD(lambda rdd: printBatch(rdd))
+f = open("filteredData","w+")
+reqInfo.foreachRDD(lambda rdd: writeToFile(rdd))
 
-reqInfo.foreachRDD(lambda rdd: rdd.foreachPartition(streamOut))
+#reqInfo.foreachRDD(lambda rdd: rdd.foreachPartition(streamOut))
 
 ssc.start()
 ssc.awaitTermination()
