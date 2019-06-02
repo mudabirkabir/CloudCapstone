6b78a7f74d52a917821ac15d357d17484a92cc86
root <root@ip-172-31-0-68.ec2.internal>
2019-06-02 05:34:05 +0000

    added sleep

--- sparkStreaming/kafkaProducer_2008.py
+++ sparkStreaming/kafkaProducer_2008.py
@@ -1,13 +1,15 @@
 import os
+import time
 from kafka import KafkaProducer
 
 # To send messages synchronously
 
-producer = KafkaProducer(bootstrap_servers=['b-2.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092','b-1.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092','b-3.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092'],batch_size=98304,linger_ms=100)
+producer = KafkaProducer(bootstrap_servers=['b-2.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092','b-1.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092','b-3.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092'],batch_size=98304,linger_ms=100,acks='all')
 
 file = open('/home/hadoop/output/filteredData_2008', 'r')
 
 for line in file:
-    producer.send('airports_2008', line)
+    producer.send('airportsAll2', line)
 
+time.sleep(10)
 file.close()
