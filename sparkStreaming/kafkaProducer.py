0f693d10ac4876cd2841923632e8805c90f31b71
mudabirkabir <mudabirkabir@gmail.com>
2019-06-07 22:50:42 -0700

    changed kafka bootstrap servers

--- sparkStreaming/kafkaProducer.py
+++ sparkStreaming/kafkaProducer.py
@@ -4,12 +4,12 @@ from kafka import KafkaProducer
 
 # To send messages synchronously
 
-producer = KafkaProducer(bootstrap_servers=['b-2.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092','b-1.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092','b-3.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092'],batch_size=98304,linger_ms=100,acks='all')
+producer = KafkaProducer(bootstrap_servers=['b-2.kafkacluster.qa2zr3.c2.kafka.us-east-1.amazonaws.com:9092','b-3.kafkacluster.qa2zr3.c2.kafka.us-east-1.amazonaws.com:9092','b-1.kafkacluster.qa2zr3.c2.kafka.us-east-1.amazonaws.com:9092'],batch_size=98304,linger_ms=100,acks='all')
 
 file = open('/home/hadoop/output/filteredData', 'r')
 
 for line in file:
     producer.send('airportsFull', line)
 
-time.sleep(10)
+time.sleep(100)
 file.close()
--- sparkStreaming/kafkaProducer_2008.py
+++ sparkStreaming/kafkaProducer_2008.py
@@ -4,12 +4,12 @@ from kafka import KafkaProducer
 
 # To send messages synchronously
 
-producer = KafkaProducer(bootstrap_servers=['b-2.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092','b-1.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092','b-3.kafkacluster.kfbj9j.c2.kafka.us-east-1.amazonaws.com:9092'],batch_size=98304,linger_ms=100,acks='all')
+producer = KafkaProducer(bootstrap_servers=['b-2.kafkacluster.qa2zr3.c2.kafka.us-east-1.amazonaws.com:9092','b-3.kafkacluster.qa2zr3.c2.kafka.us-east-1.amazonaws.com:9092','b-1.kafkacluster.qa2zr3.c2.kafka.us-east-1.amazonaws.com:9092'],batch_size=98304,linger_ms=100,acks='all')
 
 file = open('/home/hadoop/output/filteredData_2008', 'r')
 
 for line in file:
     producer.send('airportsAll2', line)
 
-time.sleep(10)
+time.sleep(100)
 file.close()
