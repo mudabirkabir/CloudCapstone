84ab221c1056fbf56b29ed9a244a6eb47ce9c043
mudabirkabir <mudabirkabir@gmail.com>
2019-06-08 18:44:11 -0700

    db query

--- delete_item.py
+++ delete_item.py
@@ -6,4 +6,9 @@ import decimal
 dynamoDB = dynamodb2.connect_to_region('us-east-1')
 dyntable = Table('Top10Carriers2', connection = dynamoDB)
 
-dyntable.delete_item(Origin='CMI')
+entries = dyntable.query_2(Origin="IAH")
+
+for entry in entries:
+    print entry
+
+
